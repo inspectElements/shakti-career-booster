6715bcea76483a0ae7c82f0fa184bd5b3db74ef0
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2024-03-09 16:53:28 +0530

    feat: landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
+import { Button } from "@material-tailwind/react";
 
 function App() {
-
-  return (
-    <>
-      <header className="App-header">
-        Shakti
-      </header>
-    </>
-  )
+    return (
+        <section className="landing-bg ">
+            <button
+                className="absolute top-[55%] left-[40%] middle none rounded-lg bg-[#1F243E] py-3 px-6 text-center align-middle font-sans text-[1.05rem] font-bold border-[#3A4065] text-white transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                data-ripple-light="true"
+            >
+                Unleash the magic of Sahayak AI
+            </button>
+        </section>
+    );
 }
 
-export default App
+export default App;
